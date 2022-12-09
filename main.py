8be7f3200749fb9959d902755ab374fca2cd0196
D1PJ <dima.zemleryb@mail.ru>
2022-12-09 18:39:54 +0300

    Расширили диапазон для пароля

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import random
 login = input('Login ')
 password = ''
 for x in range(11): #Количество символов (11)
-    password = password + random.choice(list('abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ'))
+    password = password + random.choice(list('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ'))
 print('Hello, ', login, 'your password is: ', password)
